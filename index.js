dc6cbbe2636889a901cd21d15ec5ad7a71e1246c
Yacine Rezgui <rezgui.y@gmail.com>
2014-07-24 22:37:46 +0100

    Add CORS proxy Clean code

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 var request     = require('request');
 var express     = require('express');
-var app         = express();
+var oauthApp    = express();
+var corsApp     = express();
 var bodyParser  = require('body-parser');
 var querystring = require('querystring');
 
-var port              = process.env.PORT || 1337;
+var OAUTH_PORT        = process.env.OAUTH_PORT || 1337;
+var CORS_PORT         = process.env.CORS_PORT || 9292;
 var BUFFER_OAUTH_URI  = 'https://api.bufferapp.com/1/oauth2/token.json';
 
-app.use(bodyParser());
+oauthApp.use(bodyParser());
 
-app.post('/', function (req, res) {
+oauthApp.post('/', function post(req, res) {
   if(!req.body.code) {
     res.send(400, {error: 'No authentification found'});
   }
@@ -24,8 +26,6 @@ app.post('/', function (req, res) {
     grant_type: 'authorization_code'
   };
 
-  console.log(data);
-
   return request({
     url: BUFFER_OAUTH_URI,
     method: 'POST',
@@ -33,6 +33,24 @@ app.post('/', function (req, res) {
   }).pipe(res);
 });
 
-var server = app.listen(port, function() {
-    console.log('Listening on port %d', server.address().port);
+oauthApp.listen(port, function listen() {
+    console.log('oauthApp listening on port %d', port);
 });
+
+corsApp.all('*', function all(req, res) {
+
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+
+  return request({
+    url: req.path.slice(1),
+    method: req.method,
+    qs: req.query,
+    form: req.body
+  }).pipe(res);
+});
+
+corsApp.listen(CORS_PORT, function listen() {
+    console.log('corsApp listening on port %d', CORS_PORT);
+});
+
